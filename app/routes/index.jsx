51ad22ffb8c40b74908c7ccfa2cda6ed941afa3e
maggie-j-liu <maggie.j.liu@gmail.com>
2022-02-27 18:37:25 -0800

    finish homepage

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -12,7 +12,7 @@ export default function Index() {
         <section>
           <h2 className="text-xl font-semibold">A Simple Setup</h2>
           <p>
-            Add the bot to your server using this link here:{" "}
+            To get started, add the bot to your server using this link here:{" "}
             <a
               className="text-blue-500"
               target="_blank"
@@ -36,12 +36,47 @@ export default function Index() {
           <p>
             Use the <code>/shorten</code> command to create a short link.
           </p>
+          <img
+            src="/assets/shorten-1.png"
+            alt="Use the /shorten slash command"
+            className="inline md:w-1/2"
+            width="3014px"
+            height="1756px"
+          />
+          <img
+            src="/assets/shorten-2.png"
+            alt="Results of the /shorten slash command"
+            className="inline md:w-1/2"
+            width="3014px"
+            height="1756px"
+          />
         </section>
         <section>
           <h2 className="text-xl font-semibold">Fast Redirects</h2>
           <p>
             Visit the short link the bot creates to be redirected to the
             original url.
+          </p>
+          <p>
+            Try it out!{" "}
+            <a
+              className="text-fuchsia-500 text-lg"
+              target="_blank"
+              rel="noreferrer"
+              href="https://linkr.netlify.app/maggie"
+            >
+              linkr.netlify.app/maggie
+            </a>{" "}
+            redirects to{" "}
+            <a
+              className="text-cyan-500 text-lg"
+              target="_blank"
+              rel="noreferrer"
+              href="https://maggieliu.dev"
+            >
+              maggieliu.dev
+            </a>
+            .
           </p>
         </section>
         <section>
@@ -53,6 +88,38 @@ export default function Index() {
             each link has been clicked, allowing you to have privacy-friendly
             analytics.
           </p>
+          <img
+            src="/assets/info-1.png"
+            alt="Use the /info slash command"
+            className="inline md:w-1/2"
+            width="3014px"
+            height="1756px"
+          />
+          <img
+            src="/assets/info-2.png"
+            alt="Results of the /info slash command"
+            className="inline md:w-1/2"
+            width="3014px"
+            height="1756px"
+          />
+          <p>
+            If you click the <code>Edit Link</code> button, you'll be prompted
+            to edit the url that your short link points to.
+          </p>
+          <img
+            src="/assets/edit-1.png"
+            alt="Click the edit button"
+            className="inline md:w-1/2"
+            width="3014px"
+            height="1756px"
+          />
+          <img
+            src="/assets/edit-2.png"
+            alt="After editing"
+            className="inline md:w-1/2"
+            width="3014px"
+            height="1756px"
+          />
         </section>
         <section>
           <h2 className="text-xl font-semibold">View All Links</h2>
@@ -60,6 +127,13 @@ export default function Index() {
             Want to find a list of the short links you've created? Use the{" "}
             <code>/list</code> command to see them.
           </p>
+          <img
+            src="/assets/list.png"
+            alt="Results of the /list slash command"
+            className="w-full"
+            width="3014px"
+            height="1756px"
+          />
         </section>
       </div>
     </main>
