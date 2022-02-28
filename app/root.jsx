c384cf385be67ac07ea38a47f69e70447d3af1a7
maggie-j-liu <maggie.j.liu@gmail.com>
2022-02-27 21:39:48 -0800

    meta tags

--- app/root.jsx
+++ app/root.jsx
@@ -16,7 +16,19 @@ export function links() {
 }
 
 export function meta() {
-  return { title: "Linkr: A serverless link shortener Discord bot" };
+  return {
+    title: "Linkr: A serverless link shortener Discord bot",
+    description:
+      "A serverless link shortener Discord bot built with Remix and hosted on Netlify",
+    "og:url": "https://linkr.netlify.app",
+    "og:type": "website",
+    "og:title": "Linkr: A serverless link shortener Discord bot",
+    "og:description":
+      "A serverless link shortener Discord bot built with Remix and hosted on Netlify",
+    "og:image": "/og-image.png",
+    "twitter:card": "summary_large_image",
+    keywords: "link shortener, netlify, discord, remix",
+  };
 }
 
 export default function App() {
