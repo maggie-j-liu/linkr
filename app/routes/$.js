05487c5080bd8839a00a267472761ccb0be7d34d
maggie-j-liu <maggie.j.liu@gmail.com>
2022-02-27 20:12:15 -0800

    redirect home if not found

--- app/routes/$.js
+++ app/routes/$.js
@@ -1,11 +1,11 @@
 import db from "~/utils/database.mjs";
-import { json, redirect } from "remix";
+import { redirect } from "remix";
 export const loader = async ({ request, params }) => {
   const parts = params["*"].split("/");
   const [slug, ...rest] = parts;
   const item = await db.get(slug);
   if (!item) {
-    return json("Not found", 404);
+    return redirect("/");
   }
   await db.update(
     {
